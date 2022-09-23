217d96cb84e8382f04603d152c88b6fa25bb1db3
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-09-23 22:15:49 +0900

    0923

---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+import jwt from "jsonwebtoken";
+
+export default {
+  Mutation: {
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword },
+      { loggedInUser }
+    ) => {
+      console.log(loggedInUser);
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: { id: loggedInUser },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile",
+        };
+      }
+    },
+  },
+};
