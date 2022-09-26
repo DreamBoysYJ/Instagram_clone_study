eed9feff19b7253ab85d87f9f7cf2078e97722c7
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-09-26 23:06:43 +0900

    4.10 file upload

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,39 +1,43 @@
 import bcrypt from "bcrypt";
 import client from "../../client";
-import jwt from "jsonwebtoken";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      console.log(loggedInUser);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword, bio },
+        { loggedInUser, protectResolver }
+      ) => {
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            bio,
+            ..._(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
       }
-      const updatedUser = await client.user.update({
-        where: { id: loggedInUser },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile",
-        };
-      }
-    },
+    ),
   },
 };
