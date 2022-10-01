8770c6a080c3d2e677b9ef18df6789461e544cf4
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-10-01 20:29:04 +0900

    e

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-server";
 export default gql`
   type User {
-    id: String!
+    id: Int!
     firstName: String!
     lastName: String
     username: String!
@@ -10,6 +10,7 @@ export default gql`
     updatedAt: String!
     bio: String
     avatar: String
+    photos: [Photo]
     following: [User]
     followers: [User]
     totalFollwing: Int!
