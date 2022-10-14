ad9d30777e0a2aa7a2fba9a4b43324e8a74fd341
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-10-14 22:25:04 +0900

    delete photo and comments

---
+++ comments/deleteComment/deleteComment.typeDefs.js
@@ -0,0 +1,12 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  type deleteCommentResult {
+    ok: Boolean!
+    error: String
+  }
+
+  type Mutation {
+    deleteComment(id: Int!): deleteCommentResult!
+  }
+`;
