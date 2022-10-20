011f9bb826c33d549fc8f81e6551b88452a74de5
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-10-20 16:11:35 +0900

    backend done

--- server.js
+++ server.js
@@ -12,12 +12,30 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
 
-  context: async ({ req }) => {
-    if (req) {
+  context: async (ctx) => {
+    if (ctx.req) {
       return {
-        loggedInUser: await getUser(req.headers.token),
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
       };
     }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You cann't listen");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
   },
 });
 
